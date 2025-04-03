8a83449061eda87714e9400a09d7f387049c9482
kojiro-39 <sunrise-observer_ks@live.jp>
2025-04-04 01:22:28 +0900

    Update keymap.c
        set_auto_mouse_layer(7);

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kojiro/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kojiro/keymap.c
@@ -72,6 +72,7 @@ void oledkit_render_info_user(void) {
 
 #ifdef POINTING_DEVICE_AUTO_MOUSE_ENABLE
 void pointing_device_init_user(void) {
+    set_auto_mouse_layer(7);
     set_auto_mouse_enable(true);
 }
 #endif
