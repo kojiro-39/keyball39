4c21eeca4cc2193556b84fe59ee7f99a14cace47
kojiro-39 <sunrise-observer_ks@live.jp>
2025-04-04 00:32:38 +0900

    Update config.h
    #define AUTO_MOUSE_LAYER_KEEP_TIME 30000

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kojiro/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kojiro/config.h
@@ -37,5 +37,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 7
+#define AUTO_MOUSE_LAYER_KEEP_TIME 30000
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
-#define AUTO_MOUSE_LAYER_HOLD_TIME 3000
